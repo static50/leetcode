46eefa3c6f041797bae73329f71ac3b7b84954c9
Calvin Fernandez <calvin.fernandez5138699@gmail.com>
2024-09-30 23:04:59 -0400

    new commit

---
+++ leetcode.py
@@ -0,0 +1,32 @@
+def lengthofLongestSubstring(s: str) -> int:
+    left = 0 
+    s = "abcbdefabc"
+    substr = ""
+    substrings = [] 
+    repeated_dict = dict()
+
+    for right in range(len(s)):
+        current_char = s[right]  
+
+        # if string s has a character in substr 
+        if current_char in substr: 
+            # create a substring from the last repeated character to the current repeated character
+            substr = s[left:right]  
+            print(left)
+            substrings.append(substr)      
+            # find the closest equal element to s[right] and make sure we don't include it 
+            repeated_dict[current_char] = right  
+            left = repeated_dict[current_char]
+            print(repeated_dict)
+            left += 1 
+            # create a substring from closest equal element to the other equal element
+            substr = s[left:] 
+        else:
+            substr += current_char
+              
+        
+    print(substrings) 
+         
+
+lengthofLongestSubstring("") 
+
